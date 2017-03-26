a1696ca683a64b42a24516a736a8d1cab894659f
mchogan7 <mills.c.hogan@gmail.com>
2017-03-26 16:47:53 -0500

    Responsiveness added. Ready for content

--- controllers/route_controller.js
+++ controllers/route_controller.js
@@ -21,16 +21,11 @@ var nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'serverappbeta@gmail.com',
-        pass: 'websterdog'
+        user: 'mills.c.hogan@gmail.com',
+        pass: ''
     }
 });
 
-//
-//////////////////////////////////////////////////////////////////////////////
-//SITE NAVIGATION
-//////////////////////////////////////////////////////////////////////////////
-//
 
 router.get("/", function(req, res) {
    res.render("home", req);
@@ -38,13 +33,13 @@ router.get("/", function(req, res) {
 
 
 //Nodemailer function for feedback page
-router.post("/sendFeedback", function(req, res) {
+router.post("/sendEmail", function(req, res) {
       let mailOptions = {
-        from: '"Server App Beta" <serverappbeta@gmail.com>', 
-        to: 'serverappbeta@gmail.com', 
-        subject: 'Server App Beta - Feedback', // Subject line
-        text: req.body.user_email + " comment: " + req.body.comment,
-        html: 'From: ' + req.body.user_email  + '</br><p>' + req.body.comment + '</p>'
+        from: '"Mills Message" <test@gmail.com>', 
+        to: 'mills.c.hogan@gmail.com', 
+        subject: 'Mills Hogan Send', // Subject line
+        text: req.body.email + " comment: " + req.body.message,
+        html: 'From: ' + req.body.email  + '</br><p>' + req.body.message + '</p>'
     };
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
@@ -53,10 +48,7 @@ router.post("/sendFeedback", function(req, res) {
         }
         console.log('Message %s sent: %s', info.messageId, info.response);
     });
-    var dataObject = {
-        message: "Feedback sent successfully"
-    };
-    res.render("dashboard", dataObject);
+    
 })
 
 
@@ -69,115 +61,3 @@ router.get('*', function(req, res) {
 
 // Export routes for server.js to use.
 module.exports = router;
-
-//
-//////////////////////////////////////////////////////////////////////////////
-//HELPER FUNCTIONS
-//////////////////////////////////////////////////////////////////////////////
-//
-
-//function for limiting access to logged in user only.
-//Sends them back to the login page if not.
-function loggedIn(req, res, next) {
-    if (req.user) {
-        next();
-    } else {
-        res.redirect('/login');
-    }
-}
-
-//This is used to create a temporary password.
-function tempPWgenerator() {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i = 0; i < 5; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    return text;
-}
-
-//Converts a number 0-1440 to a xx:xx:xx time.  Returns the time as a string.  Only works if num is divisible by 15.
-function convertToTime(num) {
-    var string = "";
-
-    if (num / 60 >= 23) {string+="23";}
-    else if (num / 60 >= 22) {string+="22";}
-    else if (num / 60 >= 21) {string+="21";}
-    else if (num / 60 >= 20) {string+="20";}
-    else if (num / 60 >= 19) {string+="19";}
-    else if (num / 60 >= 18) {string+="18";}
-    else if (num / 60 >= 17) {string+="17";}
-    else if (num / 60 >= 16) {string+="16";}
-    else if (num / 60 >= 15) {string+="15";}
-    else if (num / 60 >= 14) {string+="14";}
-    else if (num / 60 >= 13) {string+="13";}
-    else if (num / 60 >= 12) {string+="12";}
-    else if (num / 60 >= 11) {string+="11";}
-    else if (num / 60 >= 10) {string+="10";}
-    else if (num / 60 >= 9) {string+="09";}
-    else if (num / 60 >= 8) {string+="08";}
-    else if (num / 60 >= 7) {string+="07";}
-    else if (num / 60 >= 6) {string+="06";}
-    else if (num / 60 >= 5) {string+="05";}
-    else if (num / 60 >= 4) {string+="04";}
-    else if (num / 60 >= 3) {string+="03";}
-    else if (num / 60 >= 2) {string+="02";}
-    else if (num / 60 >= 1) {string+="01";}
-    else if (num / 60 >= 0) {string+="00";}
-
-    if (num % 60 === 0) {
-        string += ":00:00";
-    }
-    else if (num % 60 === 15) {
-        string += ":15:00";
-    }
-    else if (num % 60 === 30) {
-        string += ":30:00";
-    }
-    else if (num % 60 === 45) {
-        string += ":45:00";
-    }
-
-    return string;
-}
-
-// SEQUELIZE CRUD METHODS, FOR REFERENCE
-
-//  CREATE!
-//   db.Blah.create({
-//   email: "HELLYEAH@myspace.com",
-//   password: "superduperinsecurepassword123",
-//   age: 55,
-//   name: "Betty"
-// }).then(function(dbUser) {
-//   console.log(dbUser);
-// });
-
-//  FIND ONE!
-// db.Blah.findOne({ where: {id: 1} }).then(function(dbUser) {
-//     console.log(dbUser);
-//   });
-
-//  FIND ALL!
-// db.Blah.findAll().then(function(dbUser) {
-//   console.log(dbUser);
-// });
-
-//  UPDATE ONE!
-// var newTom = {
-//   age: 25,
-//   email: "mark@facebook.com"
-// };
-// db.Blah.update(newTom, {
-//   where: {
-//     id: 1
-//   }
-// }).then(function(dbUser) {
-//   console.log(dbUser);
-// });
-
-// DELETE ONE!
-// db.Blah.destroy({
-//   where: {id: 1}
-// }).then(function(dbUser) {
-//   console.log(dbUser);
-// });
--- public/assets/javascript/home.js
+++ public/assets/javascript/home.js
@@ -1,24 +1,135 @@
-$('li').on('click', function(){
-	var selector = $(this).text().toLowerCase()
-	var color = $('.' + selector).css('background-color')
-	if (!color){color = 'white'}
+//This has gotten out of hand.
 
-	$('.portContainer').css('height', '800px').removeClass('noAnimation')
-	$('.mask').css('height', '800px')
-	$('.' + selector).css('height', '1400px').addClass('noAnimation')
-	$('.' + selector +'Mask').css('height', '1400px')
-	$('li').css('color', 'rgba(255,255,255,.5)' )
-	$(this).css('color', color)
+$('li').on('click', function() {
+    var selector = $(this).text().toLowerCase()
+    var color = $('.' + selector).css('background-color')
+    if (!color) { color = 'white' }
+
+    $('.portContainer').css('height', '800px')
+    $('.mask').css('height', '800px')
+    $('.' + selector).css('height', '1400px').addClass('noAnimation')
+    $('.' + selector + 'Mask').css('height', '1400px')
+    $('li').css('color', 'rgba(255,255,255,.5)')
+    $(this).css('color', color)
+
+    if (selector === 'about') {
+        $('.contentHolder').css('display', 'block')
+    }
+
+    setTimeout(function() {
+        $('.portContainer').each(function() {
+            var getHeight = $(this).css('height')
+            var height = getHeight.substring(0, getHeight.length - 2)
+            if (height < 900) {
+                $(this).removeClass('noAnimation')
+            }
+        })
+
+        if (selector !== 'about') {
+            $('.contentHolder').css('display', 'none')
+        }
+
+    }, 650);
 })
 
-// $('li:contains("ABOUT")').on('click', function(){
-// 	console.log('wat')
-// 	$('.portContainer').css('height', '800px')
-// 	$('.mask').css('height', '800px')
-// 	$('li').addClass('default')
-// 	$(this).css('color', 'rgba(255,255,255,1)') 
+$('input').on('click', function(){
+	if($(this).val() === 'Email (optional)'){
+		$(this).val('')
+	}
+})
+
+$('textarea').on('click', function(){
+	if($(this).val() === 'Your message'){
+		$(this).val('')
+	}
+})
+
+$('.emailButton').on('click', function(e){
+	e.preventDefault()
+	var data = {
+		email: $('input').val(),
+		message: $('textarea').val()
+	}
+	console.log(data)
+	$.ajax({
+  type: "POST",
+  url: '/sendEmail',
+  data: data,
+  success: emailSent()
+});
+
+
+})
+
+function emailSent(){
+	$('input').val('Email (optional)'),
+	$('textarea').val('Your message')
+
+	$('.success').removeClass('vanish')
 	
-// 	setTimeout(function(){
-// 	$('.contact').removeClass('noAnimation')
-// },650);
-// })
+	setTimeout(function(){
+	$('.success').addClass('vanish')
+	}, 1000)
+}
+
+var portfolioItems = [
+	{
+		imgURL: '/images/test.jpg',
+		title: 'Test Title',
+		desc: 'This is a test description. Make sure this works with a longer paragraph. Do not go too long though.',
+		siteLink: '#',
+		gitHub: '#',
+		techIcons: ['css3', 'git', 'html5', 'illustrator', 'photoshop', 'wordpress' ,'sequelize' ,'javascript', 'jquery', 'mongodb', 'mysql', 'nodejs' ]
+
+	},
+		{
+		imgURL: '/images/test.jpg',
+		title: 'Test 2',
+		desc: 'This is asdfasdf',
+		siteLink: '#',
+		gitHub: '#',
+		techIcons: ['css3', 'git', 'html5', 'wordpress' ,'sequelize' ,'javascript', 'jquery', 'mongodb', 'mysql', 'nodejs' ]
+
+	},{
+		imgURL: '/images/test.jpg',
+		title: 'Test Title',
+		desc: 'This is a test description. Make sure this works with a longer paragraph. Do not go too long though.',
+		siteLink: '#',
+		gitHub: '#',
+		techIcons: ['css3', 'git', 'html5', 'illustrator', 'photoshop', 'wordpress' ,'sequelize' ,'javascript', 'jquery', 'mongodb', 'mysql', 'nodejs' ]
+
+	},
+		{
+		imgURL: '/images/test.jpg',
+		title: 'Test 2',
+		desc: 'This is asdfasdf',
+		siteLink: '#',
+		gitHub: '#',
+		techIcons: ['css3', 'git', 'html5', 'wordpress' ,'sequelize' ,'javascript', 'jquery', 'mongodb', 'mysql', 'nodejs' ]
+
+	}
+]
+
+createPortfolio()
+console.log(portfolioItems[0].imgURL)
+
+
+function createPortfolio() {
+	for (var i = 0; i < portfolioItems.length; i++) {
+var portMake="";
+portMake += "<div class=\"portfolioItem\">";
+portMake += "    <img class=\"portImg\" src="+ portfolioItems[i].imgURL +">";
+portMake += "    <h5>" + portfolioItems[i].title +"<\/h5>";
+portMake += "    <div class=\"itemDesc\">"+ portfolioItems[i].desc + "<\/div>";
+portMake += "    <div class=\"portLinks\"><a href="+ portfolioItems[i].siteLink + ">Visit Site<\/a><a href="+ portfolioItems[i].gitHub +"><i class=\"devicon-github-plain\"><\/i><\/a><\/div>";
+portMake += "    <div class=\"techIcons\">"
+	
+	for (var j = 0; j < portfolioItems[i].techIcons.length ; j++) {
+		portMake += "<div class=\"iconWrapper\" tooltip="+ portfolioItems[i].techIcons[j] + "><i class=\"devicon-" + portfolioItems[i].techIcons[j] + "-plain\"><\/i><\/div>";
+	}
+portMake += "<\/div>";
+portMake += "";
+
+$('.portfolioMain').append(portMake)
+	}
+}
